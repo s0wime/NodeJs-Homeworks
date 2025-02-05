b2b555e5e9600718c141da1a2a7288faf5e547f2
DAVAdjjjan <159725111+DAVAdjjjan@users.noreply.github.com>
2025-02-05 15:57:39 +0200

    prepare admin repository

---
+++ Homework 3/repository/AdminRepository.js
@@ -0,0 +1,7 @@
+const teams = require("../data/teams");
+const games = require("../data/games");
+const results = require("../data/results");
+
+class AdminRepository {}
+
+module.exports = AdminRepository;
