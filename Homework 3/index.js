bba3f2b8a089ee0dff23ac09ff0a2ac1ac4010d4
DAVAdjjjan <159725111+DAVAdjjjan@users.noreply.github.com>
2025-02-05 15:58:38 +0200

    add first endpoint

--- Homework 3/index.js
+++ Homework 3/index.js
@@ -1,7 +1,16 @@
+const guestRouter = require("./controllers/guest");
+
 const path = require("path");
 const express = require("express");
 const app = express();
 const port = 3000;
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use("/guest", guestRouter);
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
 app.listen(port, "localhost", () => {
   console.log(`http://localhost:${port}`);
