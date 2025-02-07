e72a1637b4a9dceaae95e6b9578e5ae022da7670
s0wime <158774939+s0wime@users.noreply.github.com>
2025-02-07 16:54:35 +0200

    fix game searching for admin

--- Homework 3/src/controllers/admin.js
+++ Homework 3/src/controllers/admin.js
@@ -3,14 +3,6 @@ const router = express.Router();
 const adminServiceClass = require("../services/admin");
 const adminService = new adminServiceClass();
 
-router.get("/viewSchedule/", adminService.viewSchedule);
-
-router.get("/add/", adminService.renderHelloWorld);
-router.get("/remove/", adminService.renderHelloWorld);
-
-router
-  .route("/editGame/")
-  .get(adminService.viewEditingPage)
-  .post(adminService.editGame);
+router.get("/viewSchedule/", adminService.getGames);
 
 module.exports = router;
--- Homework 3/src/services/admin.js
+++ Homework 3/src/services/admin.js
@@ -3,40 +3,21 @@ const AdminRepositoryClass = require("../repository/AdminRepository");
 const adminRepository = new AdminRepositoryClass();
 
 class AdminService {
-  viewSchedule(req, res) {
-    const schedule = adminRepository.transformGames();
-    res.render("fullSchedule", { schedule, group: "admin" });
-  }
+  getGames(req, res) {
+    const teamName = req.query.teamName;
 
-  renderHelloWorld(req, res) {
-    res.send(adminRepository.printHelloWorld());
-  }
-
-  viewEditingPage(req, res) {
-    return res.render("editGame");
-  }
-
-  editGame(req, res) {
-    const { gameId } = req.query;
-    const body = req.body;
-    console.log(body);
-    const game = adminRepository.getGameByID(gameId);
-
-    if (!game) {
-      return res.render("errorPage", { errMsg: "This game does not exist" });
+    if (!teamName) {
+      const schedule = adminRepository.transformGames();
+      return res.render("fullSchedule", { schedule, group: "admin" });
     }
 
-    if (adminRepository.getTeamByName(body.newTeamName1)) {
-      game.team1_id = newTeam1;
+    const result = adminRepository.getGamesByTeamName(teamName);
+
+    if (!result) {
+      return res.render("errorPage", { errMsg: "There is no such team." });
     }
 
-    if (adminRepository.getTeamByName(body.newTeamName2)) {
-      game.team2_id = newTeam2;
-    }
-
-    if (newDate) {
-      game.date = newDate;
-    }
+    res.render("fullSchedule", { schedule: result, group: "admin" });
   }
 }
 
