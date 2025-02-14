280e689584654b514ef9132f6ce7fb8bf33c49e9
s0wime <158774939+s0wime@users.noreply.github.com>
2025-02-15 00:11:34 +0200

    fix(hw5): make game filter work for the guest

--- Homework 5/src/services/guest.js
+++ Homework 5/src/services/guest.js
@@ -2,33 +2,62 @@ const GuestRepositoryClass = require("../repository/GuestRepository");
 const guestRepository = new GuestRepositoryClass();
 
 class GuestService {
-    async getGames(req, res) {
-        const teamName = req.query.teamName;
+  async getGames(req, res) {
+    const { teamName, dateSort, gameStatus } = req.query;
 
-        if (!teamName) {
-            guestRepository.getGames()
-                .then(schedule => {
-                    console.log(schedule)
-                  res.render("fullSchedule", {schedule, group: "guest"});
-                })
-                .catch(() => {
-                  res.render("errorPage", {errMsg: "Server Error"})
-                })
-            return;
+    if (!teamName) {
+      guestRepository
+        .getGames()
+        .then((schedule) => {
+          switch (dateSort) {
+            case "desc":
+              schedule.sort((a, b) => {
+                return new Date(a.date) - new Date(b.date);
+              });
+              break;
+            case "asc":
+              schedule.sort((a, b) => {
+                return new Date(b.date) - new Date(a.date);
+              });
+              break;
+            default:
+              break;
+          }
+
+          switch (gameStatus) {
+            case "completed":
+              schedule = [
+                ...schedule.filter((game) => game.score1 && game.score2),
+              ];
+              break;
+            case "upcoming":
+              schedule = [
+                ...schedule.filter((game) => !game.score1 && !game.score2),
+              ];
+              break;
+          }
+
+          res.render("fullSchedule", { schedule, group: "guest" });
+        })
+        .catch(() => {
+          res.render("errorPage", { errMsg: "Server Error" });
+        });
+      return;
+    }
+
+    guestRepository
+      .getGamesByTeamName(teamName)
+      .then((games) => {
+        if (!games) {
+          return res.render("errorPage", { errMsg: "No games found" });
         }
 
-        guestRepository.getGamesByTeamName(teamName)
-            .then(games => {
-                if(!games) {
-                    return res.render("errorPage", {errMsg: "No games found"})
-                }
-
-              res.render("fullSchedule", {schedule: games, group: "guest"});
-            })
-            .catch(() => {
-              res.render("errorPage", {errMsg: "Server Error"})
-            })
-    }
+        res.render("fullSchedule", { schedule: games, group: "guest" });
+      })
+      .catch(() => {
+        res.render("errorPage", { errMsg: "Server Error" });
+      });
+  }
 }
 
 module.exports = GuestService;
