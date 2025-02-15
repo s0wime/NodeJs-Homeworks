50bec448bfa9edd65099ed289ca726aa8b829389
s0wime <158774939+s0wime@users.noreply.github.com>
2025-02-15 13:49:01 +0200

    feat(hw5): add support for game filtering and search

--- Homework 5/public/scripts/editSchedule.js
+++ Homework 5/public/scripts/editSchedule.js
@@ -1,7 +1,17 @@
 const btnFilter = document.querySelector(".btn--game-filter");
-const elFilterBlock = document.querySelector(".filter-block");
-const elFilterByDate = document.querySelector(".filter-by-date");
-const elFilterByStatus = document.querySelector(".filter-by-status");
+const elFormFilter = document.querySelector(".form-game-filter");
+const elFormSearch = document.querySelector(".form-game-finder");
+
+const optionDateSortDesc = document.getElementById("date-desc");
+const optionDateSortAsc = document.getElementById("date-asc");
+
+const optionAllGames = document.getElementById("all-games");
+const optionUpcomingGames = document.getElementById("upcoming-games");
+const optionCompletedGames = document.getElementById("completed-games");
+
+const inputGameFinder = document.querySelector(".game-finder");
+
+const url = new URL(window.location);
 
 function deleteGame(id) {
   const config = {
@@ -19,5 +29,51 @@ function deleteGame(id) {
 }
 
 btnFilter.addEventListener("click", () => {
-  elFilterBlock.classList.toggle("hidden");
+  elFormFilter.classList.toggle("hidden");
+});
+
+elFormFilter.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const dateSortValue = [optionDateSortAsc, optionDateSortDesc].find(
+    (option) => option.checked
+  ).value;
+  const gameStatusValue = [
+    optionAllGames,
+    optionUpcomingGames,
+    optionCompletedGames,
+  ].find((option) => option.checked).value;
+
+  if (!url.searchParams.has("dateSort")) {
+    url.searchParams.append("dateSort", dateSortValue);
+  } else {
+    url.searchParams.set("dateSort", dateSortValue);
+  }
+
+  if (!url.searchParams.has("gameStatus")) {
+    url.searchParams.append("gameStatus", gameStatusValue);
+  } else {
+    url.searchParams.set("gameStatus", gameStatusValue);
+  }
+
+  window.location.href = url.toString();
+});
+
+elFormSearch.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const teamName = inputGameFinder.value;
+
+  if (!teamName) {
+    return;
+  }
+
+  if (!url.searchParams.has("teamName")) {
+    url.searchParams.append("teamName", teamName);
+  } else {
+    url.searchParams.set("teamName", teamName);
+  }
+
+  console.log(url.toString());
+
+  window.location.href = url.toString();
 });
